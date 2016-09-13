8a21f5c2bff3b2bdca6b8f2c09dc35eac7bc71fe
Dan Olson <olson_dan@yahoo.com>
2016-09-12 21:18:13 -0500

    Fix postgres test config and spec

--- test/integration/server/serverspec/postgres_spec.rb
+++ test/integration/server/serverspec/postgres_spec.rb
@@ -5,15 +5,15 @@ describe 'postgres installation' do
     its(:stdout) { is_expected.to match '/usr/lib/postgresql/9.4/bin/postgres -D /var/lib/postgresql/9.4/main -c config_file=/etc/postgresql/9.4/main/postgresql.conf' }
   end
 
-  describe command('psql -l on_tap_production') do
+  describe command('psql -l beermapper_production') do
     let(:db_info) do
       keys = %i(name owner encoding collate ctype access_privileges)
-      output = subject.stdout.split("\n").find { |line| line.match /^\s+?on_tap_production/ }
+      output = subject.stdout.split("\n").find { |line| line.match /^\s+?beermapper_production/ }
       Hash[keys.zip(output.split("|").map(&:strip))]
     end
 
     it 'has the correct database name' do
-      expect(db_info[:name]).to eq 'on_tap_production'
+      expect(db_info[:name]).to eq 'beermapper_production'
     end
 
     it 'has UTF8 encoding' do
