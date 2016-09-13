feea4f30b2e6cb695632109194bad774291b39a1
Dan Olson <olson_dan@yahoo.com>
2016-09-12 21:16:07 -0500

    Fix beermapper-node recipe to symlink executables correctly

--- site-cookbooks/beermapper-node/recipes/default.rb
+++ site-cookbooks/beermapper-node/recipes/default.rb
@@ -1,4 +1,14 @@
-javascript_runtime node['beermapper-node']['version']
+node_version = node['beermapper-node']['version']
+
+javascript_runtime node_version
+
+link '/usr/bin/node' do
+  to "/opt/nodejs-#{node_version}/bin/node"
+end
+
+link '/usr/bin/npm' do
+  to "/opt/nodejs-#{node_version}/bin/npm"
+end
 
 node_package 'ember-cli' do
   version node['beermapper-node']['ember-cli-version']
@@ -6,10 +16,14 @@ end
 
 node_package 'bower'
 
-link '/usr/bin/node' do
-  to '/opt/nodejs-0.10.36/bin/node'
+link '/usr/local/lib/node_modules' do
+  to "/opt/nodejs-#{node_version}/lib/node_modules"
 end
 
-link '/usr/bin/npm' do
-  to '/opt/nodejs-0.10.36/bin/npm'
+link '/usr/local/bin/bower' do
+  to "/usr/local/lib/node_modules/bower/bin/bower"
+end
+
+link '/usr/local/bin/ember' do
+  to "/usr/local/lib/node_modules/ember-cli/bin/ember"
 end
