1b1c5bc9e38d316f7aee9a5352922540e67f5ee7
Dan Olson <olson_dan@yahoo.com>
2017-08-16 20:49:22 -0500

    Add support for Mailgun config in app_config.yml

--- site-cookbooks/beermapper/attributes/default.rb
+++ site-cookbooks/beermapper/attributes/default.rb
@@ -3,5 +3,7 @@ default['beermapper']['database']['adapter'] = 'postgresql'
 default['beermapper']['database']['pool']    = 5
 default['beermapper']['database']['timeout'] = 5000
 
-default['beermapper']['app_config']['api_key']       = ''
-default['beermapper']['app_config']['secret_token']  = ''
+default['beermapper']['app_config']['api_key']         = ''
+default['beermapper']['app_config']['secret_token']    = ''
+default['beermapper']['app_config']['mailgun_api_key'] = ''
+default['beermapper']['app_config']['mailgun_domain']  = 'mail.beermapper.com'
--- site-cookbooks/beermapper/recipes/deploy.rb
+++ site-cookbooks/beermapper/recipes/deploy.rb
@@ -42,6 +42,8 @@ template '/var/apps/beermapper/shared/config/app_config.yml' do
   variables({
     rails_env: node['beermapper']['rails_env'],
     api_key: node['beermapper']['app_config']['api_key'],
-    secret_token: node['beermapper']['app_config']['secret_token']
+    secret_token: node['beermapper']['app_config']['secret_token'],
+    mailgun_api_key: node['beermapper']['app_config']['mailgun_api_key'],
+    mailgun_domain: node['beermapper']['app_config']['mailgun_domain']
   })
 end
