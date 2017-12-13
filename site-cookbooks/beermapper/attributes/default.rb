72eee755f9f240c5ea381ed80c4a6274a88b463a
Dan Olson <olson_dan@yahoo.com>
2017-12-12 21:51:04 -0600

    Update mailgun domain to mail.evergreenmenus.com

--- site-cookbooks/beermapper/attributes/default.rb
+++ site-cookbooks/beermapper/attributes/default.rb
@@ -6,4 +6,4 @@ default['beermapper']['database']['timeout'] = 5000
 default['beermapper']['app_config']['api_key']         = ''
 default['beermapper']['app_config']['secret_token']    = ''
 default['beermapper']['app_config']['mailgun_api_key'] = ''
-default['beermapper']['app_config']['mailgun_domain']  = 'mail.beermapper.com'
+default['beermapper']['app_config']['mailgun_domain']  = 'mail.evergreenmenus.com'
